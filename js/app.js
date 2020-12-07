13575740e031ae15d67418f7592084f5f2998f50
Opilane TTHK <opilane@tthk.ee>
2020-12-07 10:06:00 +0200

    commit 1

---
+++ js/app.js
@@ -0,0 +1,53 @@
+const valueName = document.querySelector('#value-name');
+const currencyName = document.querySelector('#currency-name');
+const buttonConvert = document.querySelector('#btn-convert');
+
+buttonConvert.addEventListener('click', (event) => {
+    let valueVariable = parseInt(valueName.value);;
+    let currencyVariable = currencyName.value;
+
+    let url = `https://api.coindesk.com/v1/bpi/currentprice/eur.json`;
+
+    fetch(url)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+
+            let output;
+            switch(currencyVariable){
+                case "EUR":
+                    output = data.bpi.EUR;
+                    break;
+                case "USD":
+                    output = data.bpi.USD;
+                    break;
+            }
+                
+            console.log(output);
+
+            let rateFloat = parseFloat(output.rate_float);
+            let rateMultiplier = valueVariable;
+            let rateFinal = rateFloat * rateMultiplier;
+
+            html = `
+                <h2>Results</h3>
+                <h3>Thank you! Here are the results:</h3>
+                <h4>
+                    BitCoin's value: ${rateMultiplier}<br>
+                    BitCoin's current rate: <br>
+                    ${currencyVariable} rate: ${rateFloat}<br>
+                    Value in ${currencyVariable}: ${rateFinal}
+                </h4>
+                `;
+            console.log("Value: "+rateMultiplier+"; Rate: "+rateFloat+"; Final Rate: "+rateFinal);
+            document.querySelector('.results').innerHTML = html;
+
+        })
+        .catch(error => {
+            console.log(error);
+        });
+
+    event.preventDefault();
+});
